4d8c09f5e2ece21df6d22e5d8e723fa19fa4777f
Tran Van Thuc <tranvanthucduytan@gmail.com>
2017-09-06 10:45:06 +0700

    Fix mergetypes success and waiting role, feature, ...v

--- src/schema/resolvers/permissionResolvers.js
+++ src/schema/resolvers/permissionResolvers.js
@@ -1,4 +1,23 @@
 export default {
+  Permission: {
+    // type role of permission
+    role: async ({ role_id }, data, { models: { Role } }) => {
+      const role = Role.query().findById(role_id);
+      return role;
+    },
+    // type user of permission
+    user: async ({ user_id }, data, { models: { User } }) => {
+      const user = User.query().findById(user_id);
+      return user;
+    },
+
+    feature: async ({ feature_id }, data, { models: { Feature } }) => {
+      const feature = Feature.query().findById(feature_id);
+      return feature;
+    },
+  },
+
+  // query of permission
   Query: {
     getAllPermissions: async (
       root,
@@ -27,6 +46,43 @@ export default {
         // eslint-disable-next-line no-console
         console.error(e);
         throw new ValidationError('permission not found');
+      }
+    },
+  },
+
+  //  mutation of permission
+  Mutation: {
+    insertPermission: async (
+      root,
+      data,
+      { models: { Permission }, ValidationError },
+    ) => {
+      try {
+        const permission = await Permission.query().insert(data);
+        return permission;
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+        throw new ValidationError('Cant insert permission');
+      }
+    },
+
+    // updatePermission: async (root, data, { models: { Permission } }) => {},
+
+    deletePermission: async (
+      root,
+      { id },
+      { models: { Permission }, ValidationError },
+    ) => {
+      try {
+        const permission = await Permission.query().findById(id);
+        // delete permission
+        await Permission.query().deleteById(id);
+        return permission;
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+        throw new ValidationError('Cant insert permission');
       }
     },
   },
--- src/schema/types/index.js
+++ src/schema/types/index.js
@@ -1,6 +1,7 @@
+import { mergeTypes } from 'merge-graphql-schemas';
 import userTypes from './userTypes';
 import globalTypes from './globalTypes';
 import permissionTypes from './permissionTypes';
 
 const types = [userTypes, globalTypes, permissionTypes];
-export default types;
+export default mergeTypes(types);
