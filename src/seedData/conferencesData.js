b7dae6cc7d4a1bc2d2dc811c281c2d085708bd5e
hongminhthai <minhthai0124@gmail.com>
2017-09-11 11:15:32 +0700

    Update conference and fix bugs

--- src/seedData/conferencesData.js
+++ src/seedData/conferencesData.js
@@ -3,8 +3,8 @@ const conferences = [
     title: 'Hoi nghi quoc te SigTelCom 2017',
     description:
       'International Conference on Recent Advances on Signal Processing, Telecommunications & Computing',
-    start_date: '2017-01-09 09:20:18.296-07',
-    end_date: '2017-01-011 09:20:18.296-07',
+    start_date: '2017-01-09 09:20',
+    end_date: '2017-01-011 09:20',
     bg_image: 'HoinghiquocteSigTelCom2017.png',
     user_id: '1',
     organizer_id: '1',
@@ -13,8 +13,8 @@ const conferences = [
   {
     title: 'Hoi nghi CDIO vung chau A 2017',
     description: 'Conceive Design Implement Operate',
-    start_date: '2017-03-13 10:00:18.296-08',
-    end_date: '2017-03-15 10:00:18.296-08',
+    start_date: '2017-03-13 10:00',
+    end_date: '2017-03-15 10:00',
     bg_image: 'HoinghiCDIOvungchauA2017.png',
     user_id: '2',
     organizer_id: '2',
@@ -23,8 +23,8 @@ const conferences = [
   {
     title: 'Hoi nghi Sinh viên ASEAN 2016',
     description: 'P2A – Passage to ASEAN ',
-    start_date: ' 2016-08-11 09:00:18.296-09',
-    end_date: '2016-08-14 09:00:18.296-09',
+    start_date: ' 2016-08-11 09:00',
+    end_date: '2016-08-14 09:00',
     bg_image: 'HoinghiSinhviênASEAN2016.png',
     user_id: '2',
     organizer_id: '2',
