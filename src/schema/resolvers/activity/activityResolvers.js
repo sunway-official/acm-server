3136a38a1dd2a65e2f7ee5691542eafa82381f01
Tran Van Thuc <tranvanthucduytan@gmail.com>
2017-09-16 10:00:43 +0700

    add function delete all relationship of user

--- src/schema/resolvers/activity/activityResolvers.js
+++ src/schema/resolvers/activity/activityResolvers.js
@@ -130,7 +130,6 @@ export default {
     ) => {
       try {
         const activity = await Activity.query().findById(id);
-        console.log(activity);
 
         if (!activity) throw new ValidationError('Not found Activity');
 
