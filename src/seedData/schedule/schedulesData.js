6df1592de8b475306fcddf6a1b7d8d28ed0ed56a
Tran Van Thuc <tranvanthucduytan@gmail.com>
2017-10-15 22:41:37 +0700

    Update seedData schedule

--- src/seedData/schedule/schedulesData.js
+++ src/seedData/schedule/schedulesData.js
@@ -4,44 +4,26 @@ const schedules = [
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 0
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-22 13:30',
-    end: '2017-10-22 15:30',
+    start: '2017-10-22 13:00',
+    end: '2017-10-22 16:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 1
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-22 08:30',
+    start: '2017-10-22 09:30',
     end: '2017-10-22 11:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 2
     room_id: faker.random.number({ min: 1, max: 5 }),
     start: '2017-10-22 07:30',
-    end: '2017-10-22 09:30',
+    end: '2017-10-22 09:00',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 0
     room_id: faker.random.number({ min: 1, max: 5 }),
     start: '2017-10-22 17:30',
     end: '2017-10-22 19:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 2
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-22 07:30',
-    end: '2017-10-22 09:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 0
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-22 13:30',
-    end: '2017-10-22 15:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 2
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-22 15:30',
-    end: '2017-10-22 17:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 0
@@ -53,31 +35,32 @@ const schedules = [
     activity_id: faker.random.number({ min: 1, max: 20 }), // 0
     room_id: faker.random.number({ min: 1, max: 5 }),
     start: '2017-10-23 07:30',
-    end: '2017-10-23 09:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 0
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-23 08:30',
     end: '2017-10-23 10:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 0
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-23 14:00',
-    end: '2017-10-23 16:30',
+    start: '2017-10-23 09:30',
+    end: '2017-10-23 11:30',
+  },
+  {
+    activity_id: faker.random.number({ min: 1, max: 20 }), // 1
+    room_id: faker.random.number({ min: 1, max: 5 }),
+    start: '2017-10-24 07:30',
+    end: '2017-10-24 09:00',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 0
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-24 16:30',
-    end: '2017-10-24 18:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 0
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-24 09:00',
+    start: '2017-10-24 09:30',
     end: '2017-10-24 11:30',
+  },
+
+  {
+    activity_id: faker.random.number({ min: 1, max: 20 }), // 1
+    room_id: faker.random.number({ min: 1, max: 5 }),
+    start: '2017-10-24 15:30',
+    end: '2017-10-24 18:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 1
@@ -86,22 +69,10 @@ const schedules = [
     end: '2017-10-24 19:30',
   },
   {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 1
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-24 07:30',
-    end: '2017-10-24 09:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 1
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-24 15:30',
-    end: '2017-10-24 18:30',
-  },
-  {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 2
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-25 07:30',
-    end: '2017-10-25 09:00',
+    start: '2017-10-25 08:30',
+    end: '2017-10-25 10:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 2
@@ -118,20 +89,20 @@ const schedules = [
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 2
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-25 07:00',
-    end: '2017-10-25 10:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 2
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-25 08:30',
-    end: '2017-10-25 12:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 2
-    room_id: faker.random.number({ min: 1, max: 5 }),
     start: '2017-10-25 15:30',
-    end: '2017-10-25 17:30',
+    end: '2017-10-25 17:00',
+  },
+  {
+    activity_id: faker.random.number({ min: 1, max: 20 }), // 2
+    room_id: faker.random.number({ min: 1, max: 5 }),
+    start: '2017-10-26 07:30',
+    end: '2017-10-26 10:30',
+  },
+  {
+    activity_id: faker.random.number({ min: 1, max: 20 }), // 2
+    room_id: faker.random.number({ min: 1, max: 5 }),
+    start: '2017-10-26 09:30',
+    end: '2017-10-26 11:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 1
@@ -148,32 +119,26 @@ const schedules = [
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 2
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-26 07:30',
-    end: '2017-10-26 10:30',
+    start: '2017-10-27 07:30',
+    end: '2017-10-27 11:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 2
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-26 09:30',
-    end: '2017-10-26 11:30',
+    start: '2017-10-27 13:30',
+    end: '2017-10-27 15:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 2
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-26 07:30',
-    end: '2017-10-26 11:30',
+    start: '2017-10-27 19:30',
+    end: '2017-10-27 21:30',
   },
   {
     activity_id: faker.random.number({ min: 1, max: 20 }), // 2
     room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-26 13:30',
-    end: '2017-10-26 15:30',
-  },
-  {
-    activity_id: faker.random.number({ min: 1, max: 20 }), // 2
-    room_id: faker.random.number({ min: 1, max: 5 }),
-    start: '2017-10-26 13:30',
-    end: '2017-10-26 16:30',
+    start: '2017-10-27 15:30',
+    end: '2017-10-27 17:30',
   },
 ];
 
