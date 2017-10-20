34e99ad711ea8d1ca985c5d67b9058bde7873703
Tri Pham <pvtri96@gmail.com>
2017-10-21 00:15:54 +0700

    Remove personal schedule seed

--- src/seedData/schedule/personalScheduleData.js
+++ src/seedData/schedule/personalScheduleData.js
@@ -1,40 +1,40 @@
 const personalSchedules = [
-  {
-    user_id: 4,
-    schedule_id: 4,
-    conference_id: 1,
-    activity_id: 4,
-  },
-  {
-    user_id: 4,
-    schedule_id: 7,
-    conference_id: 1,
-    activity_id: 2,
-  },
-  {
-    user_id: 4,
-    schedule_id: 5,
-    conference_id: 1,
-    activity_id: 2,
-  },
-  {
-    user_id: 4,
-    schedule_id: 3,
-    conference_id: 1,
-    activity_id: 3,
-  },
-  {
-    user_id: 9,
-    schedule_id: 1,
-    conference_id: 1,
-    activity_id: 1,
-  },
-  {
-    user_id: 9,
-    schedule_id: 6,
-    conference_id: 1,
-    activity_id: 4,
-  },
+  // {
+  //   user_id: 4,
+  //   schedule_id: 4,
+  //   conference_id: 1,
+  //   activity_id: 4,
+  // },
+  // {
+  //   user_id: 4,
+  //   schedule_id: 7,
+  //   conference_id: 1,
+  //   activity_id: 2,
+  // },
+  // {
+  //   user_id: 4,
+  //   schedule_id: 5,
+  //   conference_id: 1,
+  //   activity_id: 2,
+  // },
+  // {
+  //   user_id: 4,
+  //   schedule_id: 3,
+  //   conference_id: 1,
+  //   activity_id: 3,
+  // },
+  // {
+  //   user_id: 9,
+  //   schedule_id: 1,
+  //   conference_id: 1,
+  //   activity_id: 1,
+  // },
+  // {
+  //   user_id: 9,
+  //   schedule_id: 6,
+  //   conference_id: 1,
+  //   activity_id: 4,
+  // },
 ];
 
 module.exports = personalSchedules;
