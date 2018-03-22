132fe95d61788d05e8277e59c53eb6c1b992b713
Nhat Khanh <lednhatkhanh@gmail.com>
2018-03-22 22:41:08 +0700

    Remove more seed data

--- src/seedData/authorization/featuresData.js
+++ src/seedData/authorization/featuresData.js
@@ -1,406 +1,406 @@
-const features = [
-  // query Activity feeback
-  {
-    id: 'getAllActivityFeedback',
-  },
-  {
-    id: 'getActivityFeedbackByID',
-  },
-  {
-    id: 'getAllActivityFeedbackByUserID',
-  },
-  {
-    id: 'getAllActivityFeedbackByActivityID',
-  },
-  {
-    id: 'getAllActivityFeedbackByActivityIDUserID',
-  },
+// const features = [
+//   // query Activity feeback
+//   {
+//     id: 'getAllActivityFeedback',
+//   },
+//   {
+//     id: 'getActivityFeedbackByID',
+//   },
+//   {
+//     id: 'getAllActivityFeedbackByUserID',
+//   },
+//   {
+//     id: 'getAllActivityFeedbackByActivityID',
+//   },
+//   {
+//     id: 'getAllActivityFeedbackByActivityIDUserID',
+//   },
 
-  // mutation Activity feeback
-  {
-    id: 'insertActivityFeedback',
-  },
-  {
-    id: 'updateActivityFeedback',
-  },
-  {
-    id: 'deleteActivityFeedback',
-  },
+//   // mutation Activity feeback
+//   {
+//     id: 'insertActivityFeedback',
+//   },
+//   {
+//     id: 'updateActivityFeedback',
+//   },
+//   {
+//     id: 'deleteActivityFeedback',
+//   },
 
-  // query Papers topics
-  {
-    id: 'getAllPapersTopics',
-  },
-  {
-    id: 'getTopicsByPaperID',
-  },
+//   // query Papers topics
+//   {
+//     id: 'getAllPapersTopics',
+//   },
+//   {
+//     id: 'getTopicsByPaperID',
+//   },
 
-  // mutation Papers topics
-  {
-    id: 'insertPaperTopic',
-  },
-  {
-    id: 'updateTopicOfPaper',
-  },
-  {
-    id: 'deletePaperTopic',
-  },
+//   // mutation Papers topics
+//   {
+//     id: 'insertPaperTopic',
+//   },
+//   {
+//     id: 'updateTopicOfPaper',
+//   },
+//   {
+//     id: 'deletePaperTopic',
+//   },
 
-  // query Activity
-  {
-    id: 'getAllActivities',
-  },
-  {
-    id: 'getActivityByID',
-  },
-  {
-    id: 'getActivitiesByConferenceID',
-  },
+//   // query Activity
+//   {
+//     id: 'getAllActivities',
+//   },
+//   {
+//     id: 'getActivityByID',
+//   },
+//   {
+//     id: 'getActivitiesByConferenceID',
+//   },
 
-  // mutation Activity
-  {
-    id: 'insertActivity',
-  },
-  {
-    id: 'updateActivity',
-  },
-  {
-    id: 'deleteActivity',
-  },
+//   // mutation Activity
+//   {
+//     id: 'insertActivity',
+//   },
+//   {
+//     id: 'updateActivity',
+//   },
+//   {
+//     id: 'deleteActivity',
+//   },
 
-  // query Activity types
-  {
-    id: 'getAllActivityTypes',
-  },
-  {
-    id: 'getActivityTypeByID',
-  },
+//   // query Activity types
+//   {
+//     id: 'getAllActivityTypes',
+//   },
+//   {
+//     id: 'getActivityTypeByID',
+//   },
 
-  // mutation Activity type
-  {
-    id: 'insertActivityType',
-  },
-  {
-    id: 'updateActivityType',
-  },
-  {
-    id: 'deleteActivityType',
-  },
+//   // mutation Activity type
+//   {
+//     id: 'insertActivityType',
+//   },
+//   {
+//     id: 'updateActivityType',
+//   },
+//   {
+//     id: 'deleteActivityType',
+//   },
 
-  // delete user
-  {
-    id: 'deleteUser',
-  },
+//   // delete user
+//   {
+//     id: 'deleteUser',
+//   },
 
-  // query Conference attendee
-  {
-    id: 'getAllConferenceAttendees',
-  },
-  {
-    id: 'getConferenceAttendeeByID',
-  },
+//   // query Conference attendee
+//   {
+//     id: 'getAllConferenceAttendees',
+//   },
+//   {
+//     id: 'getConferenceAttendeeByID',
+//   },
 
-  // mutation Conference attendee
-  {
-    id: 'insertConferenceAttendee',
-  },
-  {
-    id: 'updateConferenceAttendee',
-  },
-  {
-    id: 'deleteConferenceAttendee',
-  },
+//   // mutation Conference attendee
+//   {
+//     id: 'insertConferenceAttendee',
+//   },
+//   {
+//     id: 'updateConferenceAttendee',
+//   },
+//   {
+//     id: 'deleteConferenceAttendee',
+//   },
 
-  // query Conference
-  {
-    id: 'getAllConferences',
-  },
-  {
-    id: 'getConferenceByID',
-  },
-  {
-    id: 'getConferenceByOrganizerDetailID',
-  },
-  {
-    id: 'getConferenceByAddressID',
-  },
-  {
-    id: 'getConferenceByUserID',
-  },
+//   // query Conference
+//   {
+//     id: 'getAllConferences',
+//   },
+//   {
+//     id: 'getConferenceByID',
+//   },
+//   {
+//     id: 'getConferenceByOrganizerDetailID',
+//   },
+//   {
+//     id: 'getConferenceByAddressID',
+//   },
+//   {
+//     id: 'getConferenceByUserID',
+//   },
 
-  // mutation Conference
-  {
-    id: 'insertConference',
-  },
-  {
-    id: 'updateConference',
-  },
-  {
-    id: 'deleteConference',
-  },
+//   // mutation Conference
+//   {
+//     id: 'insertConference',
+//   },
+//   {
+//     id: 'updateConference',
+//   },
+//   {
+//     id: 'deleteConference',
+//   },
 
-  // query Organizer detail
-  {
-    id: 'getAllOrganizerDetails',
-  },
-  {
-    id: 'getOrganizerDetailByID',
-  },
-  {
-    id: 'getOrganizerDetailByUserID',
-  },
+//   // query Organizer detail
+//   {
+//     id: 'getAllOrganizerDetails',
+//   },
+//   {
+//     id: 'getOrganizerDetailByID',
+//   },
+//   {
+//     id: 'getOrganizerDetailByUserID',
+//   },
 
-  // mutation Organizer detail
-  {
-    id: 'insertOrganizerDetail',
-  },
-  {
-    id: 'updateOrganizerDetail',
-  },
-  {
-    id: 'deleteOrganizerDetail',
-  },
+//   // mutation Organizer detail
+//   {
+//     id: 'insertOrganizerDetail',
+//   },
+//   {
+//     id: 'updateOrganizerDetail',
+//   },
+//   {
+//     id: 'deleteOrganizerDetail',
+//   },
 
-  // query Topics
-  {
-    id: 'getAllTopics',
-  },
-  {
-    id: 'getTopicByID',
-  },
+//   // query Topics
+//   {
+//     id: 'getAllTopics',
+//   },
+//   {
+//     id: 'getTopicByID',
+//   },
 
-  // mutation Topics
-  {
-    id: 'insertTopic',
-  },
-  {
-    id: 'updateTopic',
-  },
-  {
-    id: 'deleteTopic',
-  },
+//   // mutation Topics
+//   {
+//     id: 'insertTopic',
+//   },
+//   {
+//     id: 'updateTopic',
+//   },
+//   {
+//     id: 'deleteTopic',
+//   },
 
-  // query News comment
-  {
-    id: 'getAllNewsComments',
-  },
-  {
-    id: 'getNewsCommentByID',
-  },
-  {
-    id: 'getNewsCommentByNewsID',
-  },
-  {
-    id: 'getNewsCommentByUserID',
-  },
+//   // query News comment
+//   {
+//     id: 'getAllNewsComments',
+//   },
+//   {
+//     id: 'getNewsCommentByID',
+//   },
+//   {
+//     id: 'getNewsCommentByNewsID',
+//   },
+//   {
+//     id: 'getNewsCommentByUserID',
+//   },
 
-  // mutation News comment
-  {
-    id: 'insertNewsComment',
-  },
-  {
-    id: 'updateNewsComment',
-  },
-  {
-    id: 'deleteNewsComment',
-  },
+//   // mutation News comment
+//   {
+//     id: 'insertNewsComment',
+//   },
+//   {
+//     id: 'updateNewsComment',
+//   },
+//   {
+//     id: 'deleteNewsComment',
+//   },
 
-  // query News like
-  {
-    id: 'getAllNewsLikes',
-  },
-  {
-    id: 'getNewsLikeByID',
-  },
+//   // query News like
+//   {
+//     id: 'getAllNewsLikes',
+//   },
+//   {
+//     id: 'getNewsLikeByID',
+//   },
 
-  // mutation News like
-  {
-    id: 'insertNewsLike',
-  },
-  {
-    id: 'updateNewsLike',
-  },
-  {
-    id: 'deleteNewsLike',
-  },
+//   // mutation News like
+//   {
+//     id: 'insertNewsLike',
+//   },
+//   {
+//     id: 'updateNewsLike',
+//   },
+//   {
+//     id: 'deleteNewsLike',
+//   },
 
-  // query News photo
-  {
-    id: 'getAllNewsPhotos',
-  },
-  {
-    id: 'getNewsPhotoByID',
-  },
-  {
-    id: 'getNewsPhotoByNewsID',
-  },
+//   // query News photo
+//   {
+//     id: 'getAllNewsPhotos',
+//   },
+//   {
+//     id: 'getNewsPhotoByID',
+//   },
+//   {
+//     id: 'getNewsPhotoByNewsID',
+//   },
 
-  // mutation News photo
-  {
-    id: 'insertNewsPhoto',
-  },
-  {
-    id: 'updateNewsPhoto',
-  },
-  {
-    id: 'deleteNewsPhoto',
-  },
+//   // mutation News photo
+//   {
+//     id: 'insertNewsPhoto',
+//   },
+//   {
+//     id: 'updateNewsPhoto',
+//   },
+//   {
+//     id: 'deleteNewsPhoto',
+//   },
 
-  // query News
-  {
-    id: 'getAllNews',
-  },
-  {
-    id: 'getNewsByID',
-  },
-  {
-    id: 'getNewsByUserID',
-  },
-  {
-    id: 'getNewsByConferenceID',
-  },
+//   // query News
+//   {
+//     id: 'getAllNews',
+//   },
+//   {
+//     id: 'getNewsByID',
+//   },
+//   {
+//     id: 'getNewsByUserID',
+//   },
+//   {
+//     id: 'getNewsByConferenceID',
+//   },
 
-  // mutation News
-  {
-    id: 'insertNews',
-  },
-  {
-    id: 'updateNews',
-  },
-  {
-    id: 'deleteNews',
-  },
+//   // mutation News
+//   {
+//     id: 'insertNews',
+//   },
+//   {
+//     id: 'updateNews',
+//   },
+//   {
+//     id: 'deleteNews',
+//   },
 
-  // query Answer
-  {
-    id: 'getAllAnswers',
-  },
-  {
-    id: 'getAnswerByID',
-  },
-  {
-    id: 'getAnswerByQuestionID',
-  },
-  {
-    id: 'getAnswerByUserID',
-  },
+//   // query Answer
+//   {
+//     id: 'getAllAnswers',
+//   },
+//   {
+//     id: 'getAnswerByID',
+//   },
+//   {
+//     id: 'getAnswerByQuestionID',
+//   },
+//   {
+//     id: 'getAnswerByUserID',
+//   },
 
-  // mutation Answer
-  {
-    id: 'insertAnswer',
-  },
-  {
-    id: 'updateAnswer',
-  },
-  {
-    id: 'deleteAnswer',
-  },
+//   // mutation Answer
+//   {
+//     id: 'insertAnswer',
+//   },
+//   {
+//     id: 'updateAnswer',
+//   },
+//   {
+//     id: 'deleteAnswer',
+//   },
 
-  // query Question
-  {
-    id: 'getAllQuestions',
-  },
-  {
-    id: 'getQuestionByID',
-  },
-  {
-    id: 'getQuestionByActivityID',
-  },
-  {
-    id: 'getQuestionByUserID',
-  },
+//   // query Question
+//   {
+//     id: 'getAllQuestions',
+//   },
+//   {
+//     id: 'getQuestionByID',
+//   },
+//   {
+//     id: 'getQuestionByActivityID',
+//   },
+//   {
+//     id: 'getQuestionByUserID',
+//   },
 
-  // mutation Question
-  {
-    id: 'insertQuestion',
-  },
-  {
-    id: 'updateQuestion',
-  },
-  {
-    id: 'deleteQuestion',
-  },
+//   // mutation Question
+//   {
+//     id: 'insertQuestion',
+//   },
+//   {
+//     id: 'updateQuestion',
+//   },
+//   {
+//     id: 'deleteQuestion',
+//   },
 
-  // query Personal schedule
-  {
-    id: 'getAllPersonalSchedules',
-  },
-  {
-    id: 'getPersonalScheduleByID',
-  },
+//   // query Personal schedule
+//   {
+//     id: 'getAllPersonalSchedules',
+//   },
+//   {
+//     id: 'getPersonalScheduleByID',
+//   },
 
-  // mutation Personal schedule
-  {
-    id: 'insertPersonalSchedule',
-  },
-  {
-    id: 'updatePersonalSchedule',
-  },
-  {
-    id: 'deletePersonalSchedule',
-  },
+//   // mutation Personal schedule
+//   {
+//     id: 'insertPersonalSchedule',
+//   },
+//   {
+//     id: 'updatePersonalSchedule',
+//   },
+//   {
+//     id: 'deletePersonalSchedule',
+//   },
 
-  // query Room
-  {
-    id: 'getAllRooms',
-  },
-  {
-    id: 'getRoomByID',
-  },
+//   // query Room
+//   {
+//     id: 'getAllRooms',
+//   },
+//   {
+//     id: 'getRoomByID',
+//   },
 
-  // mutation Room
-  {
-    id: 'insertRoom',
-  },
-  {
-    id: 'updateRoom',
-  },
-  {
-    id: 'deleteRoom',
-  },
+//   // mutation Room
+//   {
+//     id: 'insertRoom',
+//   },
+//   {
+//     id: 'updateRoom',
+//   },
+//   {
+//     id: 'deleteRoom',
+//   },
 
-  // query Schedule
-  {
-    id: 'getAllSchedules',
-  },
-  {
-    id: 'getScheduleByID',
-  },
+//   // query Schedule
+//   {
+//     id: 'getAllSchedules',
+//   },
+//   {
+//     id: 'getScheduleByID',
+//   },
 
-  // mutation Schedule
-  {
-    id: 'insertSchedule',
-  },
-  {
-    id: 'updateSchedule',
-  },
-  {
-    id: 'deleteSchedule',
-  },
+//   // mutation Schedule
+//   {
+//     id: 'insertSchedule',
+//   },
+//   {
+//     id: 'updateSchedule',
+//   },
+//   {
+//     id: 'deleteSchedule',
+//   },
 
-  // query Staff in conference
-  {
-    id: 'getAllStaffInConference',
-  },
+//   // query Staff in conference
+//   {
+//     id: 'getAllStaffInConference',
+//   },
 
-  // query Co-organizer details
-  {
-    id: 'getAllCoOrganizerDetails',
-  },
-  {
-    id: 'getCoOrganizerDetailByID',
-  },
+//   // query Co-organizer details
+//   {
+//     id: 'getAllCoOrganizerDetails',
+//   },
+//   {
+//     id: 'getCoOrganizerDetailByID',
+//   },
 
-  // mutation Co-organizer detail
-  {
-    id: 'insertCoOrganizerDetail',
-  },
-  {
-    id: 'updateCoOrganizerDetail',
-  },
-  {
-    id: 'deleteCoOrganizerDetail',
-  },
-];
+//   // mutation Co-organizer detail
+//   {
+//     id: 'insertCoOrganizerDetail',
+//   },
+//   {
+//     id: 'updateCoOrganizerDetail',
+//   },
+//   {
+//     id: 'deleteCoOrganizerDetail',
+//   },
+// ];
 
-module.exports = features;
+// module.exports = features;
--- src/seedData/authorization/rolesData.js
+++ src/seedData/authorization/rolesData.js
@@ -1,28 +1,28 @@
-const roles = [
-  {
-    name: 'Organizer', // 0
-  },
-  {
-    name: 'Speaker', // 1
-  },
-  {
-    name: 'Moderator', // 2
-  },
-  {
-    name: 'Supporter', // 3
-  },
-  {
-    name: 'Ticket Checker', // 4
-  },
-  {
-    name: 'Reviewer', // 5
-  },
-  {
-    name: 'Author', // 6
-  },
-  {
-    name: 'Participant', // 7
-  },
-];
+// const roles = [
+//   {
+//     name: 'Organizer', // 0
+//   },
+//   {
+//     name: 'Speaker', // 1
+//   },
+//   {
+//     name: 'Moderator', // 2
+//   },
+//   {
+//     name: 'Supporter', // 3
+//   },
+//   {
+//     name: 'Ticket Checker', // 4
+//   },
+//   {
+//     name: 'Reviewer', // 5
+//   },
+//   {
+//     name: 'Author', // 6
+//   },
+//   {
+//     name: 'Participant', // 7
+//   },
+// ];
 
-module.exports = roles;
+// module.exports = roles;
