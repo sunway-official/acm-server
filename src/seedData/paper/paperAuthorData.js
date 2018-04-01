86db9d0ea64ad1a47f5d9e85456c648dc9e22994
tranvanthuc <tranvanthucduytan@gmail.com>
2018-04-01 16:28:17 +0700

    Updated paperAuthor, paper, paperReview seed

--- src/seedData/paper/paperAuthorData.js
+++ src/seedData/paper/paperAuthorData.js
@@ -3,9 +3,9 @@ const users = require('../authorization/userData');
 const papers = require('../paper/paperData');
 
 const paperAuthors = [];
-for (let i = 1; i <= 15; i += 1) {
+for (let i = 1; i <= 30; i += 1) {
   const authorId = faker.random.number({ min: 19, max: 21 });
-  const paperId = faker.random.number({ min: 1, max: 13 });
+  const paperId = faker.random.number({ min: 1, max: 33 });
   const author = users[authorId - 1];
   const paper = papers[paperId - 1];
   paperAuthors.push({
@@ -17,6 +17,7 @@ for (let i = 1; i <= 15; i += 1) {
     corresponding: faker.random.arrayElement([1, 2]),
     conference_id: paper.conference_id,
     paper_status: paper.status,
+    author_zipcode: faker.random.number({ min: 1000000, max: 9999999 }),
   });
 }
 
